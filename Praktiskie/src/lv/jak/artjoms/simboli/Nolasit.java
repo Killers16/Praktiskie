98608c2d94d178875a304799c404c6774c43bceb
Артём-ПК <-@DESKTOP-6A2CIVV>
2019-10-13 20:14:46 +0300

    majas

---
+++ Praktiskie/src/lv/jak/artjoms/simboli/Nolasit.java
@@ -0,0 +1,41 @@
+package lv.jak.artjoms.simboli;
+
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Nolasit {
+
+	public void writeToFile(String fileName ) {
+		File file = new File(fileName);
+		
+		
+		FileWriter fw ;
+		try {
+			if(!file.exists()) 
+			file.createNewFile();
+			//FileWriter objekts katru reizi parrakstis datnes saturu
+			//fw 	= new FileWriter(file);
+			
+			//FileWriter objekts katru reizi papildinas  datnes saturu
+			//true - norada ka datnes saturu ir japapildina(parametrs APPEND
+			//fw 	= new FileWriter(file,true);
+			
+		fw 	= new FileWriter(file,true);
+		fw.write();
+		String text = "";
+		fw.write(text+"\n");
+		fw.flush();
+		
+		fw.close();
+		System.out.println("Fails veiksmigi izveidots");
+		} catch (IOException e) {
+			System.out.println("Fails nav izveidots");
+			System.out.println("Skatit kļudas pazinojumu");
+			e.printStackTrace();
+		}
+	}
+	
+	}
+
--- Praktiskie/src/lv/jak/artjoms/simboli/U_1.java
+++ Praktiskie/src/lv/jak/artjoms/simboli/U_1.java
@@ -1,9 +1,16 @@
 package lv.jak.artjoms.simboli;
 
+import java.util.Scanner;
+
+
+
+
 public class U_1 {
 	
 	public static void main(String[] args) {
-		
+		Scanner input= new Scanner (System.in);
+		Nolasit nolasit = new Nolasit();
+		nolasit.writeToFile("Atbildes");
 		String text = "Datorsistēmu tehniķis strādā organizācijās vai pie individuāliem datorlietotājiem,"
 				+ " kur sniedz konsultācijas darbā ar datortehniku, "
 				+ "programmatūru un biroja tehniku, uzstāda, "
@@ -20,10 +27,11 @@ public class U_1 {
 		System.out.println("1.teksta garums.");
 		
 		System.out.println("Dota teksta garums ir "+text.length()+" Simboli.");
+		
 		System.out.println("2.Noteikt, vai teksts satur vārdu (programmatūru) – ignorēt lielos burtus");
 		String text2 = text.toLowerCase();
 		System.out.println("ar IF ELSE");
-		System.out.println("Teksts satur vārdu \"programatūru\" :"+text2.contains("programmatÅ«ru"));
+		System.out.println("Teksts satur vārdu \"programatūru\" :"+text2.contains("programmatūru"));
 		
 		if(text2.contains("Programaturu")) {
 			System.out.println("Teksts satur vārdu \"programmatūru\"");
@@ -54,7 +62,9 @@ public class U_1 {
 		System.out.println();
 		System.out.println("9Noteikt apakšvirknes tekstu,"
 				+ " ja apakšvirknes 1. simbola indekss ir 13,"
-				+ " bet pēdējais apakšvirknes simbols ir teksta 42. simbols tekstā.");
+				+ "\n bet pēdējais apakšvirknes "
+				+ "\nsimbols ir teksta 42. simbols "
+				+ "\ntekstā:");
 		System.out.println(text.substring(13, 43));
 		System.out.println();
 		System.out.println("10. Sadalīt simbolu virkni pa vārdiem.");
@@ -84,34 +94,56 @@ public class U_1 {
 	System.out.println("12.Atrast īsāko vārdu. "
 			+ "Ja vairākiem vārdiem vienāds garums, "
 			+ "izvadīt tos visus. ");
-	String text12 = text.replace(",", "");
-	text12 = text.replace(".", "");
+	System.out.println("pedejais vards ir : " 
+			+ text.substring (text.lastIndexOf (' '), text.length()));
 	
-	String[] vardi12 = text.split("");
-	String isais=vardi12[0];
-	String visiIsie=vardi12[0];
-	 
-	for(String vards1 : vardi12) {
-		 if(vards1.length()<isais.length()) {
-			isais = vards1;
-			 visiIsie = vards1;
-		 }
-		 else if(vards1.length()==isais.length()) {
-			 visiIsie += ( ","+vards1);
-		 }
-		 
-	 }
-	System.out.println("Visi isakie vardi ir"+visiIsie);
 	System.out.println();
 	System.out.println("13.Noteikt, kuri vārdi sākas ar burtu "
 			+ "„K” – ignorēt lielo burtu rakstību. "
 			+ "Izvadīt visus tos vārdus.");
 	
+	String[] burts= text.split(" K");
+
+	 for(int i=0; i<burts.length; i++) {
+	   String[] vards = burts[i].split(" ");
+
+	    if( i > 0 ) {
+	    	// ignorējiet pārējos vārdus, jo nesācies ar K
+	      System.out.println("K" + vards[0]); 
+	    }
+	else {    //  Pārbaude 1. skaitli
+	          for(int j=0; j<vards.length; j++) {
+
+	        if ( vards[j].startsWith("k") || vards[j].startsWith("K"))
+	              System.out.println(vards[j]); 
+	            }   
+	        }
+
+	     }
+	 System.out.println("14. Pārveidot vārdu „Datorsistēmu” uz lieliem burtiem");
+	 String vards  = "datorsistēmu"; 
+	 System.out.println(vards = vards.substring(0,12).toUpperCase() + vards.substring(12).toLowerCase());
+	 
+	 System.out.println("15. Noteikt, kuri vārdi beidzas ar burtu „s”. Izvadīt visus tos vārdus. (3 punkti)");
+	 String[] burts1= text.split(" s");
+
+	 for(int i=0; i<burts1.length; i++) {
+	   String[] vards1 = burts1[i].split(" ");
+
+	  
+	{    
+	          for(int j=0; j<vards1.length; j++) {
+
+	        if ( vards1[j].endsWith("s") || vards1[j].endsWith("s"))
+	              System.out.println(vards1[j]); 
+	            }   
+	        }
+
+	     }
+
+	 
 	
-	
-	
-	
-	
+	input.close();
 	}
 	
 
