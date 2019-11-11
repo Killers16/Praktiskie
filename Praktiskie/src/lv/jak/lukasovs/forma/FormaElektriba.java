f5a3088b6060610933efedd95e219bf41a02b480
Артём-ПК <-@DESKTOP-6A2CIVV>
2019-11-11 21:14:47 +0200

    Дома делал форму надо написать; кнопку зделать сохронить файле

--- Praktiskie/src/lv/jak/lukasovs/forma/FormaElektriba.java
+++ Praktiskie/src/lv/jak/lukasovs/forma/FormaElektriba.java
@@ -1,5 +1,9 @@
 package lv.jak.lukasovs.forma;
 
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -8,19 +12,23 @@ import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
+
 public class FormaElektriba extends JFrame {
 	
-	JLabel VardsL,UzvardsL,EpastsL,RekinaNrL,TalrunisL,PatēretājsL,ApekinsL;
+	JLabel VardsL,UzvardsL,EpastsL,RekinaNrL,TalrunisL,PatēretājsL,ApekinsL,KilovataL;
 	
 	JList<String>listedNAmes;
-	JTextField VardsTF,UzvardsTF,EpastsTF,RekinaNrTF,TalrunisTF,PatēretājsTF,ApekinsTF;
-	JButton addBTN,ClearBTN;
+	JTextField VardsTF,UzvardsTF,EpastsTF,RekinaNrTF,TalrunisTF,PatēretājsTF;
+	JButton AcceptBTN;
 	JPanel panel;
+	
+	DefaultListModel<String> availableModel = new DefaultListModel<String>();
+	DefaultListModel<String> selectedModel = new DefaultListModel<String>();
 	
 	DefaultListModel<String>model = new DefaultListModel<String>();
 	
 	public FormaElektriba() {
-		setBounds(600, 150, 400, 300);
+		setBounds(600, 150, 300, 400);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setTitle("Elektrības apreķināšana");
 		setResizable(false);
@@ -30,56 +38,70 @@ public class FormaElektriba extends JFrame {
 		setContentPane(panel);
 		
 	}
+	
+	
 	public void init() {
 		panel = new JPanel(null);
 		
 		VardsL = new JLabel("Vārds");
-		VardsL.setBounds(10, 10, 80, 20);
+		VardsL.setBounds(50, 10, 80, 30);
 		
 		VardsTF = new JTextField();
-		VardsTF.setBounds(90, 10, 80, 20);
+		VardsTF.setBounds(100, 10,160, 30);
 		
 		UzvardsL = new JLabel("Uzvārds");
-		UzvardsL.setBounds(10, 40, 80, 30);
+		UzvardsL.setBounds(40, 50, 80, 30);
 		
 		UzvardsTF = new JTextField();
-		VardsTF.setBounds(50, 10, 120, 20);
-		
-		RekinaNrL = new JLabel("Rēķina Nr -");
-		RekinaNrL.setBounds(10,70, 80, 30);
-		
-		RekinaNrTF = new JTextField();
-		RekinaNrTF.setBounds(90, 70, 80, 20);
+		UzvardsTF.setBounds(100, 50, 160, 30);
 		
 		EpastsL = new JLabel("E-pasts:");
-		EpastsL.setBounds(10,100, 80, 30);
+		EpastsL.setBounds(40,90, 80, 30);
 		
 		EpastsTF = new JTextField();
-		EpastsTF.setBounds(90, 100, 80, 20);
+		EpastsTF.setBounds(100, 90, 160, 30);
 		
-		TalrunisL = new JLabel("Talrunis :");
-		TalrunisL.setBounds(10,120, 80, 30);
+		RekinaNrL = new JLabel("Rēķina Nr:");
+		RekinaNrL.setBounds(30,130, 90, 30);
+		
+		RekinaNrTF = new JTextField();
+		RekinaNrTF.setBounds(100, 130, 160, 30);
+		
+		TalrunisL = new JLabel("Talrunis:");
+		TalrunisL.setBounds(40,170, 80, 30);
 		
 		TalrunisTF = new JTextField();
-		TalrunisTF.setBounds(90, 120, 80, 20);
+		TalrunisTF.setBounds(100, 170, 160, 30);
 		
 		PatēretājsL = new JLabel("kWh:");
-		PatēretājsL.setBounds(10,150, 80, 30);
+		PatēretājsL.setBounds(60,210, 80, 30);
 		
 		PatēretājsTF = new JTextField();
-		PatēretājsTF.setBounds(90, 150, 80, 20);
+		PatēretājsTF.setBounds(100, 210, 160, 30);
 		
-		ApekinsL = new JLabel("Apreķina dati:");
-		ApekinsL.setBounds(10,180, 80, 30);
+		String s = null;
+		ApekinsL = new JLabel("Apreķina dati: ");
+		ApekinsL.setBounds(10,250, 130, 30);
 		
-		ApekinsTF = new JTextField();
-		ApekinsTF.setBounds(90, 180, 80, 20);
 		
-		panel.add(VardsL);panel.add(UzvardsL);panel.add(RekinaNrL);panel.add(EpastsL);
-		panel.add(TalrunisL);panel.add(PatēretājsL);panel.add(ApekinsL);
 		
-		panel.add(VardsTF);panel.add(UzvardsTF);panel.add(RekinaNrTF);panel.add(EpastsTF);
-		panel.add(TalrunisTF);panel.add(PatēretājsTF);panel.add(ApekinsTF);
+		KilovataL = new JLabel("1 kWh = 0.05029"); 
+		KilovataL.setBounds(120,280, 100, 30);
+		
+		AcceptBTN = new JButton("Accept");
+		AcceptBTN.setBounds(100, 330, 100, 30);
+		
+		
+				
+			
+		
+		panel.add(VardsL);panel.add(UzvardsL);panel.add(EpastsL);panel.add(RekinaNrL);panel.add(TalrunisL);
+		panel.add(PatēretājsL);panel.add(ApekinsL);panel.add(KilovataL);
+		
+		panel.add(AcceptBTN);
+		
+		panel.add(VardsTF);panel.add(UzvardsTF);panel.add(EpastsTF);panel.add(RekinaNrTF);panel.add(TalrunisTF);
+		panel.add(PatēretājsTF);
 	}
 
 
